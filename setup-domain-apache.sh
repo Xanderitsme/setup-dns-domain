ffcb691ddc184f8a7c9fdd9b7d9e6511810a883f
Xanderistme <xanderitsme@gmail.com>
2024-11-06 14:32:17 -0500

    Fix: incorrect config site file indentation

--- setup-domain-apache.sh
+++ setup-domain-apache.sh
@@ -2,27 +2,27 @@
 
 # Solicitar información al usuario
 
-while [ -z "$DOMINIO" ]; do
+while [ -z "$DOMAIN" ]; do
 read -p "Introduce el dominio (ejemplo: mysite.com): " DOMINIO
-if [ -z "$DOMINIO" ]; then
+if [ -z "$DOMAIN" ]; then
 echo "El dominio no puede estar vacío. Por favor, introduce un valor."
 fi
 done
 
-read -p "Introduce el subdominio (ejemplo: www): " SUBDOMINIO
+read -p "Introduce el subdominio (ejemplo: www): " SUBDOMAIN
 
 # Establecer un valor por defecto si el subdominio esta vacio
 
-SUBDOMINIO="${SUBDOMINIO:-www}"
+SUBDOMAIN="${SUBDOMAIN:-www}"
 
 # Crear el directorio raíz
 
-echo "Creando el directorio raíz /var/www/$DOMINIO"
-mkdir -p /var/www/$DOMINIO
+echo "Creando el directorio raíz /var/www/$DOMAIN"
+mkdir -p /var/www/$DOMAIN
 
 # Crear el archivo raíz para el sitio
 
-ROOT_DOCUMENT="/var/www/$DOMINIO/index.php"
+ROOT_DOCUMENT="/var/www/$DOMAIN/index.php"
 echo "Creando el archivo raíz $ROOT_DOCUMENT"
 cat <<EOF > "$ROOT_DOCUMENT"
 
@@ -31,40 +31,41 @@ cat <<EOF > "$ROOT_DOCUMENT"
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width">
-  <title>$DOMINIO</title>
+  <title>$DOMAIN</title>
 </head>
 <body>
-	<p>Este el sitio web de $DOMINIO</p>
+	<p>Este el sitio web de $DOMAIN</p>
 </body>
 EOF
 
 # Crear el archivo de configuración para el sitio
 
-CONFIG_FILE="/etc/apache2/sites-available/$DOMINIO.conf"
+CONFIG_FILE="/etc/apache2/sites-available/$DOMAIN.conf"
 echo "Creando el archivo de configuración para el sitio $CONFIG_FILE"
 cat <<EOF > "$CONFIG_FILE"
 <VirtualHost *:80>
-  ServerName unamad.joomla.com
-  DirectoryIndex index.html index.php
-  DocumentRoot /var/www/html/Joomla
-  ErrorLog /joomla-error.log
-  CustomLog /joomla-access.log combined
-  <Directory /var/www/html/Joomla>
-    Options FollowSymLinks
-    AllowOverride All
-    Require all granted
-  </Directory>
+    ServerName $SUBDOMAIN.$DOMAIN
+    DirectoryIndex index.html index.php
+    DocumentRoot /var/www/$DOMAIN
+    ErrorLog ${APACHE_LOG_DIR}/$DOMAIN-error.log
+    CustomLog ${APACHE_LOG_DIR}/$DOMAIN-access.log combined
+
+    <Directory /var/www/$DOMAIN>
+        Options FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
 </VirtualHost>
 EOF
 
 # Habilitar el sitio en Apache
 
 echo "Habilitando el sitio en Apache"
-a2ensite $DOMINIO.conf
+a2ensite $DOMAIN.conf
 
 # Recargar Apache para aplicar los cambios
 
 echo "Recargando Apache para aplicar la nueva configuración"
 systemctl reload apache2
 
-echo "El sitio $DOMINIO con subdominio $SUBDOMINIO ha sido configurado correctamente en: http://$SUBDOMINIO.$DOMINIO"
+echo "El sitio $DOMAIN con subdominio $SUBDOMAIN ha sido configurado correctamente en: http://$SUBDOMAIN.$DOMAIN"
